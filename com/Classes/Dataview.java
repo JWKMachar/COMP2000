884412c93d7a9a43a1e9bde9b645264e08df98b5
JWKMachar <57765783+JWKMachar@users.noreply.github.com>
2021-01-10 15:25:05 +0000

    Load Data into Admin Dataview

--- com/Classes/Dataview.java
+++ com/Classes/Dataview.java
@@ -1,10 +1,44 @@
 package Classes;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Dataview {
     private JPanel panel1;
     private JLabel lbl;
+    private JComboBox comboBox1;
+    private JLabel lblCurrentStock;
+    private JTextField textField1;
+    private JButton confirmNewStockButton;
+    private JButton LOADDATAButton;
+    private JButton selectStockButton;
+
+    public Dataview() {
+        confirmNewStockButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                
+            }
+        });
+        LOADDATAButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for (int i = 0; i < CheckOut.ShopStock.totalProducts; i = i + 1) {
+                    comboBox1.addItem(CheckOut.ShopStock.Products[i].Name);
+                }
+            }
+        });
+        selectStockButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                lblCurrentStock.setText(String.valueOf(CheckOut.ShopStock.Amount[comboBox1.getSelectedIndex()]));
+            }
+        });
+    }
+
 
     public static void main(String[] args)
     {
