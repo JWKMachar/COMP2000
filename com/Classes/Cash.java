6ad587862b2505daf0b1da90291b2dd084b90c92
JWKMachar <57765783+JWKMachar@users.noreply.github.com>
2021-01-11 02:39:59 +0000

    Receipt mostly done
    Need to finish writing the receipt before it can be written to the Receipt List

--- com/Classes/Cash.java
+++ com/Classes/Cash.java
@@ -15,17 +15,27 @@ public class Cash {
 
 
     public Cash() {
+
+
+
         confirmButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e)
             {
                 double ChangeRequired = Double.parseDouble(textField1.getText()) - Double.parseDouble(lblTotalCost.getText());
-
                 DecimalFormat df = new DecimalFormat("#.00");
                 String ChangeRequiredDF = df.format(ChangeRequired);
-
                 lblChangeRequired.setText(String.valueOf(ChangeRequiredDF));
+
+                int length = CheckOut.thisOrder.Length();
+                for (int i = 0; i < length; i++) {
+                    int ItemCode = Integer.parseInt(CheckOut.thisOrder.ReceiptCode.getItem(i));
+                    CheckOut.ShopStock.Amount[ItemCode] =  CheckOut.ShopStock.Amount[ItemCode] - 1;
+                }
+                Main.pushData(CheckOut.ShopStock);
+                Receipt.main(null);
             }
+
         });
         loadButton.addActionListener(new ActionListener() {
             @Override
--- com/Classes/Receipt.java
+++ com/Classes/Receipt.java
@@ -1,10 +1,32 @@
 package Classes;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Receipt {
 
     private JPanel panel1;
+    private JList list1;
+    private JButton loadButton;
+
+    public Receipt() {
+        loadButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                int length = CheckOut.thisOrder.Length();
+                for (int i = 0; i < length; i++) {
+                    int ItemCode = Integer.parseInt(CheckOut.thisOrder.ReceiptCode.getItem(i));
+                    String ItemName = CheckOut.ShopStock.getItem(ItemCode).Name;
+                    double ItemPrice = CheckOut.ShopStock.getItem(ItemCode).Price;
+                    String AddItem = ItemName + " " + ItemPrice;
+                    list1.setListData(AddItem);
+
+                }
+            }
+        });
+    }
 
     public static void main(String[] args)
     {
