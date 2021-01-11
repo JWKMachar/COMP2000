f530bb30dc450c872edc0a9bea02461bcd949ee7
JWKMachar <57765783+JWKMachar@users.noreply.github.com>
2021-01-11 15:59:14 +0000

    Declined Card Payments fixed
    - declined card payments now takes you to cash payment

--- com/Classes/Decline.java
+++ com/Classes/Decline.java
@@ -1,10 +1,21 @@
 package Classes;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Decline {
     private JButton declinedButton;
     private JPanel panel1;
+
+    public Decline() {
+        declinedButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Cash.main(null);
+            }
+        });
+    }
 
     public static void main(String[] args)
     {
